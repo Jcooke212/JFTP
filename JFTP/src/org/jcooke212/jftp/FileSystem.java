7957e2a61e6773182d6dd72046f4e6fe9b1d8ed9
Jcooke212 <jcooke212@gmail.com>
2013-01-17 17:12:46 -0500

    Fragment Type
    Change the fragment type to ListFragment

--- JFTP/src/org/jcooke212/jftp/FileSystem.java
+++ JFTP/src/org/jcooke212/jftp/FileSystem.java
@@ -1,28 +1,26 @@
 package org.jcooke212.jftp;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.Gravity;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
 
-public class FileSystem extends Fragment 
+public class FileSystem extends ListFragment 
 {
-	/**
-	 * The fragment argument representing the section number for this
-	 * fragment.
-	 */
+    /**
+     * The fragment argument representing the section number for this fragment.
+     */
+    
+    public static final String ARG_DISPLAY_TYPE = "display_type";
+    
+    public FileSystem(){}
 
-	public FileSystem()
-	{}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
-	{
-		TextView textView = new TextView(getActivity());
-		textView.setGravity(Gravity.CENTER);
-		return textView;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) 
+    {
+	ListView listView = (ListView) getActivity().findViewById(R.id.list);
+	return listView;
+    }
 }
