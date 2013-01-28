1fb7720ac4a518ffa83654665d8414b14b67523c
jcooke212 <jcooke212@gmail.com>
2013-01-28 13:24:43 -0500

    Add Activity
    Add new activity for settings

---
+++ JFTP/src/org/jcooke212/jftp/JFTP_settings.java
@@ -0,0 +1,18 @@
+package org.jcooke212.jftp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JFTP_settings extends Activity 
+{
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) 
+	{
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
